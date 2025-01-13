fe3b4c1277cfdcc9f495f2956df2d7f8ce08db5c
Basab Dattaray <dattaraybasab0@gmail.com>
2025-01-12 19:06:17 -0800

    built added .env

--- main.go
+++ main.go
@@ -1,20 +1,30 @@
-// main.go
-
 package main
 
 import (
-	"os"
+    "log"
+    "os"
 
-	"github.com/dattaray-basab/oauth-example/oauth"
+    "github.com/dattaray-basab/oauth-example/oauth"
+    "github.com/joho/godotenv"
 )
 
 func main() {
-	// Call the RunOauth function with parameters
+    // Load environment variables from .env file
+    err := godotenv.Load()
+    if err != nil {
+        log.Fatalf("Error loading .env file: %v", err)
+    }
 
-	oauth.RunOauth(":8080", map[string]string{
-		"clientID":     os.Getenv("GOOGLE_CLIENT_ID"),
-		"clientSecret": os.Getenv("GOOGLE_CLIENT_SECRET"),
-		"redirectURL":  "http://localhost:8080/callback",
-	})
+    // Get values from environment variables
+    port := os.Getenv("PORT")
+    clientID := os.Getenv("CLIENT_ID")
+    clientSecret := os.Getenv("CLIENT_SECRET")
+    redirectURL := os.Getenv("REDIRECT_URL")
 
+    // Call the RunOauth function
+    oauth.RunOauth(port, map[string]string{
+        "clientID":     clientID,
+        "clientSecret": clientSecret,
+        "redirectURL":  redirectURL,
+    })
 }
